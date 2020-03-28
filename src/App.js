e904c4c4f642b3c80a2acb6fd2ae2d48071c9334
unknown <bgajdkos@gmail.com>
2020-03-28 12:52:48 +0100

    setup webpack

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {hot} from 'react-hot-loader';
+import './App.css';
+
+const App=()=>(<div className="App">Hello, world!!!</div>);
+
+export default hot(module)(App);
